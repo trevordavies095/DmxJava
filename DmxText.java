25d1b0e68918502bc55c72ea648babf5cf8dc2d4
ltd9938 <ltd9938@rit.edu>
2018-09-17 10:45:50 -0400

    Added test

---
+++ DmxText.java
@@ -0,0 +1,20 @@
+public DmxTest {
+	public static void main(String[] args) {
+		// Local constants
+		
+		// Local variables
+		DmxJava dmx = new DmxJava();
+		byte[] dmxPacket;
+		
+		/****** start DmxTest() ******/
+		dmx.setChannel(1, 255);
+		dmx.setChannel(289, 155);
+		dmxPacket = dmx.render();
+		dmx.printPacket(dmxPacket);
+		
+		dmx.setChannel(90, 0);
+		dmx.blackout();
+		dmxPacket = dmx.render();
+		dmx.printPacket(dmxPacket);
+	}
+}
